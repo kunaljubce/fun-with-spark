e5fa60e3b37b9a548931ebe99b3538be14475c80
Kunal Bhattacharya <kunal.jubce@gmail.com>
2018-12-30 21:33:56 +0530

    committing modified transactions file containing code for non matched transactions

--- code/transactions.py
+++ code/transactions.py
@@ -9,28 +9,27 @@ if __name__ == "__main__":
 
 	trans08RDD = trans08.map(lambda line: (line.split(",")[1], line.split(",")[2], line.split(",")[3]))
 	trans09RDD = trans09.map(lambda line: (line.split(",")[1], line.split(",")[2], line.split(",")[3]))
+	# grouping the RDDs based on all 3 elements and collecting the values
 	trans08RDDGrouped = trans08RDD.groupBy(lambda k: (k[0],k[1],k[2])).map(lambda x : (x[0], list(x[1]))).collect()
 	trans09RDDGrouped = trans09RDD.groupBy(lambda k: (k[0],k[1],k[2])).map(lambda x : (x[0], list(x[1]))).collect()
-	
+	# converting the lists, returned after groupBy, to RDDs again
 	trans08RDDGroupedRDD = sc.parallelize(trans08RDDGrouped)
 	trans09RDDGroupedRDD = sc.parallelize(trans09RDDGrouped)
 
 	transCombined = trans08RDDGroupedRDD.cogroup(trans09RDDGroupedRDD)
-	print(type(transCombined))
+	# print(type(transCombined))
 	transCombinedFiltered = transCombined.filter(lambda x: ((list(x[0]), list(x[1])) if len(list(x[1][1])) > 0 and len(list(x[1][0])) > 0 else ""))
-																																																																																																																																																																												   		transCombinedFiltered.mapValues(lambda x: (list(x[0]), list(x[1]))).coalesce(1).saveAsTextFile("/home/hduser/Desktop/spark/matchingTransactions")
+																																																																																																																																																																															  
+	transCombinedFiltered.mapValues(lambda x: (list(x[0]), list(x[1]))).coalesce(1).saveAsTextFile("/home/hduser/Desktop/spark/matchingTransactions")
 
 	# To find the accounts with transactions in only 1 month
 	trans08RDDGroupedAcOnly = trans08RDD.groupBy(lambda k: k[0]).map(lambda x : (x[0], list(x[1]))).collect()
 	trans09RDDGroupedAcOnly = trans09RDD.groupBy(lambda k: k[0]).map(lambda x : (x[0], list(x[1]))).collect()
 	trans08RDDGroupedAcOnlyRDD = sc.parallelize(trans08RDDGroupedAcOnly)
 	trans09RDDGroupedAcOnlyRDD = sc.parallelize(trans09RDDGroupedAcOnly)
-	transUnique = trans08RDDGroupedAcOnlyRDD.subtractByKey(trans09RDDGroupedAcOnlyRDD)
+	trans08Unique = trans08RDDGroupedAcOnlyRDD.subtractByKey(trans09RDDGroupedAcOnlyRDD)
+	trans09Unique = trans09RDDGroupedAcOnlyRDD.subtractByKey(trans08RDDGroupedAcOnlyRDD)
+	transUniqueTotal = trans08Unique.union(trans09Unique)
+	transUniqueTotal.mapValues(lambda x: (list(x[0]))).coalesce(1).saveAsTextFile("/home/hduser/Desktop/spark/nonMatchingTransactions")	
 
-	#print(trans08RDD.top(10))	
-	#print(transCombinedAndGrouped.collect().top(10))
-	#print(transCombinedAndGrouped.mapValues(lambda x: (list(x[0]), list(x[1]))))
-	#common_trans = trans08RDD.cogroup(trans09RDD)
-	#transCombinedAndGrouped = transCombined.groupBy(lambda k: k[0])
-	#print ([(k, list(v)) for (k, v) in transCombined.take(1)])
 
